63227208a347fa03330b180ceeef82f24dd9bf72
GiangHT <95273832+perplexedG3@users.noreply.github.com>
2022-10-28 11:02:28 +0700

    Update login.php

--- login.php
+++ login.php
@@ -6,48 +6,46 @@ session_start();
 header('Content-Type: text/html; charset=UTF-8');
 
 //handle login
-if (isset($_POST['login'])) {
-    //connect to database
-    include('connect.php');
 
-    //get input
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+//connect to database
+include('connect.php');
 
-    //check enter password and username
-    if (!$username || !$password) {
-        echo "Please enter the full login username and password!";
-        exit;
-    }
+//get input
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-    $regex1 = preg_match('/[\'"^£$%&*()}{@#~?><>,|=_+¬-]/', $username);
-    $regex2 = preg_match('/[\'"^£$%&*()}{@#~?><>,|=_+¬-]/', $password);
-    if (!$regex1 && !$regex2) { 
-        //hash pasword
-        $password = md5($password);
-
-        $query = "SELECT username,password FROM users WHERE username = ? and md5(password) = '$password'";
-
-        // use prepared statement to prevent SQL injection
-        $preparedStatement = $connect->prepare($query);
-        $preparedStatement->bind_param('s', $username); 
-        $preparedStatement->execute();
-        $result = $preparedStatement->get_result();
-
-        if (mysqli_num_rows($result) <= 0) {
-            echo "Incorrect username or password!";
-        } else {
-            //lưu tên đăng nhập
-            $_SESSION['username'] = $username;
-            $_SESSION["loggedin"] = true;
-            echo "abc";
-            // header("location: challenges.php");
-        }
-    }
-    else{
-        http_response_code(400);
-        die('Error processing bad or malformed request');
-    }
-
-    $connect->close();
+//check enter password and username
+if (!$username || !$password) {
+    echo "Please enter the full login username and password!";
+    exit;
 }
+
+$regex1 = preg_match('/[\'"^£$%&*()}{@#~?><>,|=_+¬-]/', $username);
+$regex2 = preg_match('/[\'"^£$%&*()}{@#~?><>,|=_+¬-]/', $password);
+if (!$regex1 && !$regex2) {
+    //hash pasword
+    $password = md5($password);
+
+    $query = "SELECT username,password FROM users WHERE username = ? and md5(password) = '$password'";
+
+    // use prepared statement to prevent SQL injection
+    $preparedStatement = $connect->prepare($query);
+    $preparedStatement->bind_param('s', $username);
+    $preparedStatement->execute();
+    $result = $preparedStatement->get_result();
+
+    if (mysqli_num_rows($result) <= 0) {
+        echo "Incorrect username or password!";
+    } else {
+        //lưu tên đăng nhập
+        $_SESSION['username'] = $username;
+        $_SESSION["loggedin"] = true;
+
+        header("location: challenges.html");
+    }
+} else {
+    http_response_code(400);
+    die('Error processing bad or malformed request');
+}
+
+$connect->close();
